60179c0f9a04dcd44ac2e703e1f61acaa7f34acd
Thomas Simader <thomas.simader@geomecon.de>
2025-04-30 17:07:20 +0200

    map on control mesh is running!

--- scripts/C_map_on_control_mesh.py
+++ scripts/C_map_on_control_mesh.py
@@ -3,38 +3,15 @@ import sys
 import numpy as np
 import pyvista as pv
 from pathlib import Path
-import pandas as pd
-from helpers import safe_parse_quantity, delete_pyvista_fields, map_on_control_mesh
-import vtk
+sys.path.append(str(Path(__file__).parents[1]))
+from scr.utils import (create_control_mesh,
+                       map_on_control_mesh,
+                       delete_comsol_fields)
+from scr.comsol_module.comsol_classes import COMSOL_VTU
 from tqdm import tqdm
-from typing import Tuple
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-from ComsolClasses.comsol_classes import COMSOL_VTU
-from ComsolClasses.helper import calculate_normal
 
 
-def create_control_mesh(bounds: pv.BoundsLike,
-                        spacing: Tuple[int | float, int | float, int | float]) -> vtk.vtkImageData:
-    """Generates a structured grid withtin "bounds" and spacing in x, y, z directions specified in "spacing".
 
-    Args:
-        bounds (pv.BoundsLike): _description_
-        spacing (Tuple[int  |  float, int  |  float, int  |  float]): _description_
-
-    Returns:
-        vtk.vtkImageData: _description_
-    """
-    dx, dy, dz = spacing  # You can increase this for more resolution
-    # Compute the number of points needed (+ 1 as spacing is an interval)
-    nx = int((bounds[1] - bounds[0]) / dx) + 1
-    ny = int((bounds[3] - bounds[2]) / dy) + 1
-    nz = int((bounds[5] - bounds[4]) / dz) + 1
-
-    image = vtk.vtkImageData()
-    image.SetDimensions(nx, ny, nz)
-    image.SetOrigin(bounds[0], bounds[2], bounds[4])
-    image.SetSpacing(dx, dy, dz)
-    return image
 
 
 def main():   
@@ -42,19 +19,45 @@ def main():
     PARAMETER_SPACE = "03"
     DATA_TYPE = "Training"
     FIELDS_TO_EXPORT : List[str] = ["Temperature"]
-    TIME_STEPS_TO_EXPORT : List[str | int] = [-1]  
 
-    data_folder = Path(ROOT / "Snapshots" / PARAMETER_SPACE /  "Training_Original") # data_type) #"Truncated") # data_type)    
+    data_folder = Path(ROOT / "data" / PARAMETER_SPACE /  "Training_Original") # data_type) #"Truncated") # data_type)    
     assert data_folder.exists(), f"Data folder {data_folder} does not exist."
-    export_folder =  data_folder.parent / DATA_TYPE
+    export_folder =  data_folder.parent / (DATA_TYPE + "Mapped")
+    export_folder.mkdir(exist_ok=True)
     assert export_folder.exists(), f"Export folder {export_folder} does not exist."
     assert str(data_folder.absolute()) != str(export_folder.absolute()), "Import and Export from same folder not allowed"
 
     vtu_files = sorted([path for path in data_folder.iterdir() if path.suffix == ".vtu"])
     
-    bounds = COMSOL_VTU(vtu_files[0]).mesh.bounds
-    control_mesh = create_control_mesh(bounds)
-
+    x_min, x_max, y_min, y_max, z_min, z_max = COMSOL_VTU(vtu_files[0]).mesh.bounds
+    spacing = (50, 50, 50)
+    
+    # Reduce bounds of control mesh to be within source mesh to avoid interpolation errors.
+    x_min = int(x_min) + spacing[0]
+    x_max = int(x_max) - spacing[0]
+    y_min = int(y_min) + spacing[1]
+    y_max = int(y_max) - spacing[1]
+    z_min = int(z_min) + spacing[2] # z_min is negativ
+    # z_max = int(z_max) - spacing[2] # z_max is positive
+    
+    control_mesh = create_control_mesh((x_min, x_max,
+                                        y_min, y_max,
+                                        z_min, z_max),
+                                       spacing)
+    
+    for vtu_path in tqdm(vtu_files, total=len(vtu_files), desc="Mapping files on control mesh"):
+        comsol_data = COMSOL_VTU(vtu_path)
+        idx = comsol_data.mesh.field_data["Idx"]
+        # Delete fields of no interest to reduce file size after interpolation
+        comsol_data = delete_comsol_fields(comsol_data, FIELDS_TO_EXPORT)
+        for i in range(len(comsol_data.times) - 1): # delete every time step except last one (-1)
+            comsol_data.mesh.point_data.remove(comsol_data.format_field("Temperature", i))
+        mapped : pv.ImageData = map_on_control_mesh(comsol_data.mesh, control_mesh)
+        assert np.min(mapped.point_data["vtkValidPointMask"]) > 0, f"Error in interpolation in file {vtu_path.name}"
+        mapped.point_data.remove("vtkValidPointMask")
+        spacing_str = '_'.join(f"{x:.0f}" for x in spacing)
+        bounds_str = '_'.join(f"{x:.0f}" for x in (x_min, x_max, y_min, y_max, z_min, z_max))
+        mapped.save(export_folder / f"{vtu_path.stem}_s{spacing_str}_b{bounds_str}.vti") # VTK ImageData (best for structured image data)
 
 if __name__ == "__main__":
     main()

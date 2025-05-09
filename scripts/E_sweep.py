72e9b4d04e33be2340fc4fd36a26b0bcd2585ca8
Thomas Simader <thomas.simader@geomecon.de>
2025-05-09 10:57:11 +0200

    WIP: local changes to E_sweep.py

--- scripts/E_sweep.py
+++ scripts/E_sweep.py
@@ -14,7 +14,7 @@ from scr.utils import load_pint_data
 
 def objective(trial: optuna.Trial) -> float:
     # Architecture: variable number of layers and neurons per layer
-    hidden1 = trial.suggest_int("hiden1", low = 2, high = 25)
+    hidden1 = trial.suggest_int("hiden1", low = 2, high = 50)
     num_inbetw_layers = trial.suggest_int("num_inbetw_layers", 1, 5)
     hidden_layers_betw = [
         trial.suggest_int(f"hidden_layers_betw{i}", 25, 300, step=2)
@@ -23,13 +23,12 @@ def objective(trial: optuna.Trial) -> float:
     
     hidden6 = trial.suggest_int("hiden6", low = 10,  high = 100 ,step=2)
     
-    ROOT = Path("/Users/thomassimader/Documents/NIRB/Snapshots/01")
     N_EPOCHS = 25_000 #20_000
     ACCURACY = 1e-3
     
     # Other hyperparameters
-    lr = trial.suggest_float("lr", 1e-5, 2.5e-3, log=True)
-    batch_size = trial.suggest_int("batch_size", 15, 25)
+    lr = trial.suggest_float("lr", 1e-5, 5e-3, log=True)
+    batch_size = trial.suggest_int("batch_size", 10, 100)
 
     activation_name = "sigmoid" # trial.suggest_categorical("activation", ["relu", "leaky_relu", "sigmoid", "tanh"])
     if activation_name == "leaky_relu":
@@ -42,14 +41,13 @@ def objective(trial: optuna.Trial) -> float:
         activation_fn = nn.Tanh()
 
 
-    ROOT = Path(__file__).parents[1] / "data" / "01"
+    control_mesh_suffix =  "s100_100_100_b0_4000_0_5000_-4000_0"
     assert ROOT.exists(), f"Not found: {ROOT}"
-    basis_func_path = ROOT / "BasisFunctions" / f"basis_fts_matrix_{ACCURACY:.1e}.npy"
-    train_snapshots_path = ROOT / "Exports" / "Training_temperatures.npy"
-    test_snapshots_path = ROOT / "Test" / "Test_temperatures.npy"
+    basis_func_path = ROOT / "TrainingMapped" / control_mesh_suffix / "BasisFunctions" / f"basis_fts_matrix_{ACCURACY:.1e}.npy"
+    train_snapshots_path = ROOT / "TrainingMapped" / control_mesh_suffix / "Exports" / "Training_temperatures.npy"
+    test_snapshots_path = ROOT / "TestMapped" / control_mesh_suffix / "Exports" / "Test_temperatures.npy"
     train_param_path = ROOT / "training_samples.csv"
     test_param_path = ROOT / "test_samples.csv"
-    
     
     basis_functions         = np.load(basis_func_path)
     training_snapshots      = np.load(train_snapshots_path)
@@ -88,7 +86,7 @@ def objective(trial: optuna.Trial) -> float:
             
     optuna_pruning = MyEarlyStopping(
         trial, 
-        min_epochs=8_000,
+        min_epochs=15_000,
         monitor="train_loss",        # or "val_loss"
         mode="min",                  # we're minimizing loss
         )
@@ -98,13 +96,13 @@ def objective(trial: optuna.Trial) -> float:
                                       data_module.basis_func_mtrx)
 
 
-    logger = TensorBoardLogger(ROOT / "Optuna_Logs_2", name=f"trial_{trial.number}")
+    # logger = TensorBoardLogger(ROOT / "Optuna_Logs_2", name=f"trial_{trial.number}")
     trainer = L.Trainer(max_epochs=N_EPOCHS,
                         logger=False,
                         enable_checkpointing=False,
                         callbacks=[optuna_pruning, r2_callback],
                         enable_progress_bar=False,
-                        max_time={"minutes": 30},
+                        max_time={"minutes": 50},
                         )
 
     trainer.fit(model, train_dataloaders=data_module.train_dataloader(shuffle=True))
@@ -116,14 +114,15 @@ def objective(trial: optuna.Trial) -> float:
     return train_loss
 
 if __name__ == "__main__":
-    db_path = Path(__file__).parents[1] / "db.sqlite3"
+    ROOT = Path(__file__).parents[1] / "data" / "03"
+    db_path = ROOT / "db.sqlite3"
     storage_param = {
         "storage": f"sqlite:///{db_path}",  # Specify the storage URL here.
-        "study_name": "optuna_sweep_6",
+        "study_name": "optuna_sweep_1",
         "load_if_exists": True
     }
     study = optuna.create_study(direction="minimize", **storage_param)
-    study.optimize(objective, n_trials=200, n_jobs=4)
+    study.optimize(objective, n_trials=400, n_jobs=5)
 
     print("Best hyperparameters:")
     print(study.best_params)

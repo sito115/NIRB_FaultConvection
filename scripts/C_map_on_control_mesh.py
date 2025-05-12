c123d593f1632d5a705af67cd29403d17a802ff3
Thomas Simader <thomas.simader@geomecon.de>
2025-05-12 11:12:40 +0200

    Included assertation

--- scripts/C_map_on_control_mesh.py
+++ scripts/C_map_on_control_mesh.py
@@ -13,7 +13,7 @@ from scr.utils import (create_control_mesh,
                        inverse_distance_weighting)
 from scr.comsol_module.comsol_classes import COMSOL_VTU
 from tqdm import tqdm
-logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def handle_invalid_point_mask(target_point: np.ndarray,
@@ -35,12 +35,13 @@ def handle_invalid_point_mask(target_point: np.ndarray,
 
 def main():   
     ROOT = Path(__file__).parents[1]
-    PARAMETER_SPACE = "03"
-    DATA_TYPE = "Training"
+    PARAMETER_SPACE = "01"
+    DATA_TYPE = "Test"
     FIELD_TO_EXPORT : str = "Temperature"
-    SPACING = (50, 50, 50) # dx, dy, dz
+    SPACING = (100, 100, 100) # dx, dy, dz
+    DECIMAL_TOLERANCE = 1
 
-    data_folder = Path(ROOT / "data" / PARAMETER_SPACE /  "Training_Original") # data_type) #"Truncated") # data_type)    
+    data_folder = Path(ROOT / "data" / PARAMETER_SPACE /  "Test") # data_type) #"Truncated") # data_type)    
     assert data_folder.exists(), f"Data folder {data_folder} does not exist."
     export_folder =  data_folder.parent / (DATA_TYPE + "Mapped")
     export_folder.mkdir(exist_ok=True)
@@ -49,16 +50,23 @@ def main():
 
     # Find vtu files of original snapshots
     vtu_files = sorted([path for path in data_folder.iterdir() if path.suffix == ".vtu"])
-    original_comsol_bounds = COMSOL_VTU(vtu_files[0]).mesh.bounds
     
-    control_mesh = create_control_mesh(original_comsol_bounds,
-                                       SPACING)
+    original_mesh_path = Path("/Users/thomassimader/Documents/NIRB/data/01/Training/Training_000.vtu")
+    
+    original_comsol_bounds = COMSOL_VTU(original_mesh_path).mesh.bounds
+    
+    # control_mesh = create_control_mesh(original_comsol_bounds,
+    #                                    SPACING)
+    control_mesh = COMSOL_VTU(original_mesh_path).mesh
+    control_mesh.clear_data()
     
     # Clip bounds of control mesh to avoid interpolation errors
     logging.debug(f"Old bounds of control mesh: {control_mesh.bounds}")
+    logging.debug(f"Old n_points of control mesh: {control_mesh.n_points}")
     bbox = pv.Box(np.trunc(original_comsol_bounds)) # converts image data to unstructered grid
     control_mesh = control_mesh.clip_box(bbox, invert=False)
     logging.debug(f"New bounds of control mesh: {control_mesh.bounds}")
+    logging.debug(f"New n_points of control mesh: {control_mesh.n_points}")
     
     # create export folder with spacing and bounds information
     spacing_str = '_'.join(f"{x:.0f}" for x in SPACING)
@@ -73,28 +81,33 @@ def main():
         
         # Delete fields of no interest to reduce file size after interpolation
         comsol_data = delete_comsol_fields(comsol_data, [FIELD_TO_EXPORT])
-        for i in range(len(comsol_data.times) - 1): # delete every time step except last one (-1)
-            comsol_data.mesh.point_data.remove(comsol_data.format_field(FIELD_TO_EXPORT, i))
+        # for i in range(len(comsol_data.times) - 1): # delete every time step except last one (-1)
+        #     comsol_data.mesh.point_data.remove(comsol_data.format_field(FIELD_TO_EXPORT, i))
         mapped : pv.ImageData = map_on_control_mesh(comsol_data.mesh, control_mesh)
-        field_name = comsol_data.mesh.point_data.keys()[-1]
         
         validity_array = mapped.point_data['vtkValidPointMask']  
         invalid_indices = np.where(validity_array == 0)[0]
-        
-        # If there are invalid points, loop over them 
         if invalid_indices.size > 0:
-            logging.warning(f"\t Found {invalid_indices.size} invalid points in {vtu_path.name}")
-            for idx in invalid_indices:
-                invalid_point = mapped.points[idx]
-                interpolated_value = handle_invalid_point_mask(invalid_point, comsol_data.mesh, field_name)
-                mapped.point_data[field_name][idx] = interpolated_value
-
-        tolerance = 1
-        assert np.round(np.min(mapped.point_data[field_name]), tolerance) >= np.round(np.min(comsol_data.mesh.point_data[field_name]), tolerance), f"Mapped minimum ({np.min(mapped.point_data[field_name]):.3e}) is smaller than original minimum ({np.min(comsol_data.mesh.point_data[field_name]):.2e})"
-        assert np.round(np.max(mapped.point_data[field_name]), tolerance) <= np.round(np.max(comsol_data.mesh.point_data[field_name]), tolerance), f"Mapped maximum ({np.max(mapped.point_data[field_name]):.3e}) is bigger than original minimum  ({np.max(comsol_data.mesh.point_data[field_name]):.2e})"
-        assert np.sum(np.isnan(mapped.point_data[field_name])) == 0, "Found nan-values in mapped mesh"
-        mapped.point_data.remove("vtkValidPointMask")
+            logging.warning(f"\t Found {invalid_indices.size} invalid points in of {vtu_path.name}")
+            for field_name in comsol_data.mesh.point_data.keys():
+                if field_name == "vtkValidPointMask":
+                    continue
+                
+                # If there are invalid points, loop over them 
+                for idx in invalid_indices:
+                    invalid_value = mapped.point_data[field_name][idx] 
+                    if invalid_value >= np.min(comsol_data.mesh.point_data[field_name]) and invalid_value <= np.max(comsol_data.mesh.point_data[field_name]):
+                        logging.debug(f"\t\t Skipped {field_name}")
+                        continue
+                    invalid_point = mapped.points[idx]
+                    interpolated_value = handle_invalid_point_mask(invalid_point, comsol_data.mesh, field_name)
+                    mapped.point_data[field_name][idx] = interpolated_value
+                
+                assert np.round(np.min(mapped.point_data[field_name]), DECIMAL_TOLERANCE) >= np.round(np.min(comsol_data.mesh.point_data[field_name]), DECIMAL_TOLERANCE), f"Mapped minimum ({np.min(mapped.point_data[field_name]):.3e}) is smaller than original minimum ({np.min(comsol_data.mesh.point_data[field_name]):.2e})"
+                assert np.round(np.max(mapped.point_data[field_name]), DECIMAL_TOLERANCE) <= np.round(np.max(comsol_data.mesh.point_data[field_name]), DECIMAL_TOLERANCE), f"Mapped maximum ({np.max(mapped.point_data[field_name]):.3e}) is bigger than original minimum  ({np.max(comsol_data.mesh.point_data[field_name]):.2e})"
+                assert np.sum(np.isnan(mapped.point_data[field_name])) == 0, "Found nan-values in mapped mesh"
         
+        mapped.point_data.remove("vtkValidPointMask")
         # Transfer meta data (Parameters, Idx, SimTime)
         for key, value in comsol_data.mesh.field_data.items(): 
             mapped.field_data[key] = value
